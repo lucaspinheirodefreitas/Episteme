1210365b487ef03d9c6a1e4d4d66fb0134762384
lucaspinheirodefreitas <lucaspinheirodefreitas@gmail.com>
2019-11-20 15:55:50 -0300

    Ajuste na página inicial.

--- src/br/com/episteme/dao/UsuarioDAO.java
+++ src/br/com/episteme/dao/UsuarioDAO.java
@@ -2,17 +2,12 @@ package br.com.episteme.dao;
 
 import java.util.List;
 
-import br.com.episteme.model.Usuario;
-
 public class UsuarioDAO implements GenericDAO{
 	public void create(Object o) {
 		
 	}
 	public List<Object> read(Object o) {
-		Usuario usuario = new Usuario();
-		usuario.setNome("Lucas");
-		// ajustar os atributos necessários:
-		return (List<Object>) usuario;
+		return null;
 	}
 	public void update(Object o) {
 		
